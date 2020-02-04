335da76281398994c166dfed7932591762debe98
Nobuo Kawaguchi <kawaguti@nagoya-u.jp>
2020-02-04 19:03:43 +0900

    add file check

--- build-mac.js
+++ build-mac.js
@@ -1,4 +1,19 @@
 const builder = require('electron-builder');
+// check there is synerex dir and
+//   there is binary file
+//     nodeserv
+//     synerex-server
+//     harmovis-layers
+
+const fs = require('fs')
+
+if (!(fs.existsSync('synerex/nodeserv') && 
+     fs.existsSync('synerex/synerex-server') &&
+     fs.existsSync('synerex/harmovis-layers'))) {
+	console.log("You should put binaries into synerex dir")
+	process.exit(-1)
+}
+
 
 builder.build({
     config: {
